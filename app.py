9ae7e385db70792c6f41707f342c9bb9598432ce
EliteGamer007 <150921203+EliteGamer007@users.noreply.github.com>
2025-05-07 20:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class XOs:
         if 0 <= row < 3 and 0 <= col < 3:
             if self.space[row][col] == ' ':
                 self.space[row][col] = player
-                self.full+=1
+                self.full += 1
                 return True
             else:
                 print("Space already occupied.")
@@ -18,40 +18,52 @@ class XOs:
         else:
             print("Invalid row or column.")
             return False
+
     def win(self):
         for i in range(3):
             if self.space[i][0] == self.space[i][1] == self.space[i][2] != ' ':
-                return self.space[i][0]  
+                return self.space[i][0]
             if self.space[0][i] == self.space[1][i] == self.space[2][i] != ' ':
-                return self.space[0][i] 
+                return self.space[0][i]
 
         if self.space[0][0] == self.space[1][1] == self.space[2][2] != ' ':
             return self.space[0][0]
         if self.space[0][2] == self.space[1][1] == self.space[2][0] != ' ':
             return self.space[0][2]
         return None
+
     def draw(self):
-        if self.full==9:
+        if self.full == 9:
             print("The game is a draw")
-            return None
-        return None  
+            return True
+        return False
+
+    def print_board(self):
+        print("Current Board:")
+        for row in self.space:
+            print(" | ".join(row))
+            print("-" * 9)
+
+
 print("Play Noughts and Crosses")
 game = XOs()
 while True:
-    print("Player 1 Enter row and column number to place '"'X'"'")
-    r1=int(input("Row: "))
-    c1=int(input("Column: "))
-    game.insert('X',r1-1,c1-1)
-    key1=game.win()
-    if key1 is not None:
-        print("Player 1 is the winner")
-        break
-    game.draw()
-    print("Player 2 Enter row and column number to place '"'O'"'")
-    r2=int(input("Row: "))
-    c2=int(input("Column: "))
-    game.insert('O',r2-1,c2-1)
-    key2=game.win()
-    if key2 is not None:
-        print("Player 2 is the winner")
-        break add a print function to showcase the board
+    print("Player 1 Enter row and column number to place 'X'")
+    r1 = int(input("Row: "))
+    c1 = int(input("Column: "))
+    if game.insert('X', r1 - 1, c1 - 1):
+        game.print_board()
+        if game.win() == 'X':
+            print("Player 1 is the winner")
+            break
+        if game.draw():
+            break
+
+    print("Player 2 Enter row and column number to place 'O'")
+    r2 = int(input("Row: "))
+    c2 = int(input("Column: "))
+    if game.insert('O', r2 - 1, c2 - 1):
+        game.print_board()
+        if game.win() == 'O':
+            print("Player 2 is the winner")
+            break
